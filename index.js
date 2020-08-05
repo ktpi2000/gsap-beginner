edc0efd9d18c80755f5884dc8c3e40782540a66e
ktpi2000 <katapi2000@gmail.com>
2020-08-05 15:35:53 +0900

    fade in

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 import { gsap } from 'gsap';
 
-gsap.to("h1", 1, {x:100})
+gsap.to("h1", 1, {x: 100})
+
+gsap.from(".hello", {
+    y: 50,
+    opacity: 0,
+    duration: 0.3,
+  })
+
+gsap.to(".nyan", 1, {x: 300, y: 100, scale: 1.5})
